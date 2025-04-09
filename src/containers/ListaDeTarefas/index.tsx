26fa2d01976c523e2966272c99232542245bf32f
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-04-09 00:56:35 -0300

    feat: Parametrizando cards

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,23 +1,24 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
 
 const tarefas = [
   {
     titulo: 'Estudar Typescript',
-    prioridade: 'Impotante',
-    status: 'Pendente',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE,
     descricao: 'Estudar Typescript para melhorar o conhecimento em React'
   },
   {
     titulo: 'Pagar a Conta de Intenet',
-    prioridade: 'Urgente',
-    status: 'Concluída',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA,
     descricao: 'Baixar a fatura no gmail e pagar no banco'
   },
   {
     titulo: 'Ir a academia',
-    prioridade: 'Impotante',
-    status: 'Pendente',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE,
     descricao: 'Fazer musculação e aeróbico por 1 hora'
   }
 ]
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,6 @@
+export default {
+  vermelho: '#c23616',
+  verde: '#44bd32',
+  amareloClaro: '#f0c11a',
+  amareloEscuro: '#e1a32a'
+}
