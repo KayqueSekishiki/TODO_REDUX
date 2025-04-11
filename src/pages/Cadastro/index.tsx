a32c3a0849c12e28cce7fc9ce267f2b1034d4751
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-04-11 00:36:10 -0300

    feat: react router dom

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,8 @@
+const Cadastro = () => (
+  <>
+    <h1>Cadastro</h1>
+    <h2>Novo item</h2>
+  </>
+)
+
+export default Cadastro
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   font-family: Roboto, sans-serif;
   list-style: none;
+  text-decoration: none;
   }
 `
 
